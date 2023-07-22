6b0e548fe3ca0d41dd6aff85e982069f4e5fc775
kimxons <kitongameshack9@gmail.com>
2023-07-23 01:01:33 +0300

    modelling file update - ploting feature importance

--- data_help/modelling.py
+++ data_help/modelling.py
@@ -1,9 +1,10 @@
 import platform
-from typing import Union
+from typing import Union, List, Optional
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+import seaborn as sns  # Added seaborn for feature importance plot
 from sklearn.metrics import (accuracy_score, classification_report, confusion_matrix, f1_score, precision_score,
                              recall_score, roc_auc_score, roc_curve)
 from sklearn.model_selection import cross_val_score
@@ -17,9 +18,9 @@ else:
 def train_classifier(
     X_train: Union[pd.DataFrame, np.ndarray],
     y_train: Union[pd.Series, np.ndarray],
-    X_val: Union[pd.DataFrame, np.ndarray],
-    y_val: Union[pd.Series, np.ndarray],
     estimator: object,
+    X_val: Optional[Union[pd.DataFrame, np.ndarray]] = None,
+    y_val: Optional[Union[pd.Series, np.ndarray]] = None,
     cross_validate: bool = False,
     cv: int = 5,
 ) -> None:
@@ -29,26 +30,18 @@ def train_classifier(
     Args:
         X_train: The feature set (X) to use in training an estimator to predict the outcome (y).
         y_train: The ground truth value for the training dataset.
-        X_val: The feature set (X) to use in validating a trained estimator.
-        y_val: The ground truth value for the validation dataset.
         estimator: The estimator to be trained and evaluated.
+        X_val: The feature set (X) to use in validating a trained estimator (optional).
+        y_val: The ground truth value for the validation dataset (optional).
         cross_validate: Whether to use a cross-validation strategy.
         cv: Number of folds to use in cross-validation.
 
     Returns:
         None
     """
-    if X_train is None:
-        raise ValueError("X_train is None")
-
-    if y_train is None:
-        raise ValueError("y_train is None")
-
-    if X_val is None:
-        raise ValueError("X_val is None")
-
-    if y_val is None:
-        raise ValueError("y_val is None")
+    # Check for None inputs
+    if any(arg is None for arg in [X_train, y_train, X_val, y_val]):
+        raise ValueError("Some input arguments are None.")
 
     if cross_validate:
         scorers = [
@@ -78,39 +71,38 @@ def train_classifier(
             plt.xlabel("False Positive Rate")
             plt.ylabel("True Positive Rate")
             plt.title("Receiver Operating Characteristic Curve")
+            plt.legend()
 
 
-# def plot_feature_importance(estimator=None, col_name=None):
-#     """
-#     Plots the feature importance from a trained scikit learn estimator
-#     as a bar chart.
+def plot_feature_importance(estimator: object, feature_names: List[str]) -> plt.Figure:
+    """
+    Plots the feature importance from a trained scikit learn estimator
+    as a bar chart.
 
-#     Parameters:
-#     -----------
-#     estimator : scikit-learn estimator
-#         A fitted estimator that has a `feature_importances_` attribute.
-#     col_names : list of str
-#         The names of the columns in the same order as the feature importances.
+    Parameters:
+    -----------
+    estimator : scikit-learn estimator
+        A fitted estimator that has a `feature_importances_` attribute.
+    feature_names : list of str
+        The names of the columns in the same order as the feature importances.
 
-#     Returns:
-#     --------
-#     fig : matplotlib Figure
-#         The figure object containing the plot.
-#     ax : matplotlib Axes
-#         The axes object containing the plot.
-#     """
+    Returns:
+    --------
+    fig : matplotlib Figure
+        The figure object containing the plot.
+    """
 
-#     if not hasattr(estimator, "feature_importances_"):
-#         raise ValueError("The estimator does not have a 'feature_importances_' attribute.")
-#     if not isinstance(feature_names, list) or len(feature_names) != estimator.n_features_:
-#         raise ValueError("The 'feature_names' argument should be a list of the same length as the number of features.")
+    if not hasattr(estimator, "feature_importances_"):
+        raise ValueError("The estimator does not have a 'feature_importances_' attribute.")
+    if not isinstance(feature_names, list) or len(feature_names) != estimator.n_features_:
+        raise ValueError("The 'feature_names' argument should be a list of the same length as the number of features.")
 
-#     feature_importances = estimator.feature_importances_
-#     feature_importances_df = pd.DataFrame({"feature": feature_names, "importance": feature_importances})
-#     feature_importances_df = feature_importances_df.sort_values(by="importance", ascending=False)
+    feature_importances = estimator.feature_importances_
+    feature_importances_df = pd.DataFrame({"feature": feature_names, "importance": feature_importances})
+    feature_importances_df = feature_importances_df.sort_values(by="importance", ascending=False)
 
-#     fig, ax = plt.subplots()
-#     sns.barplot(x="importance", y="feature", data=feature_importances_df, ax=ax)
-#     ax.set_title("Feature importance plot")
+    fig, ax = plt.subplots()
+    sns.barplot(x="importance", y="feature", data=feature_importances_df, ax=ax)
+    ax.set_title("Feature importance plot")
 
-#     return fig, ax
+    return fig
