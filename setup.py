aea214c72ec43a87c809aca49b95bd06c4ed8c5f
kimxons <kitongameshack9@gmail.com>
2023-07-26 12:21:30 +0300

    modified the setup file

--- setup.py
+++ setup.py
@@ -1,44 +1,39 @@
-# import io
-# from os.path import dirname, join
+#!/usr/bin/env python3
 
-# from setuptools import find_packages, setup
+import os
+from setuptools import find_packages, setup
 
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
 
-# def read(*names, **kwargs):
-#     try:
-#         with io.open(join(dirname(__file__), *names), encoding=kwargs.get("encoding", "utf8")) as fr:
-#             return fr.read()
-#     except (IOError, UnicodeError) as e:
-#         print(f"Error reading the file: {e}")
-#         return None
-
-
-# setup(
-#     name="datahelp",
-#     version="0.1.0",
-#     license="MIT",
-#     description="A python library for easy modelling",
-#     long_description=read("README.md"),
-#     long_description_content_type="text/markdown",
-#     author="Meshack Kitonga",
-#     author_email="dev.kitonga@gmail.com",
-#     url="https://github.com/datahelp",
-#     packages=find_packages(),
-#     install_requires=[
-#         "pandas",
-#         "scikit-learn",
-#         "seaborn",
-#         "numpy",
-#         "jupyter",
-#         "matplotlib",
-#         "nltk",
-#         "joblib",
-#     ],
-#     classifiers=[
-#         "Development Status :: 3 - Alpha",
-#         "License :: OSI Approved :: MIT License",
-#         "Intended Audience :: Developers",
-#         "Programming Language :: Python :: 3",
-#     ],
-#     python_requires=">=3.6",
-# )
+setup(
+    name="datahelp",
+    version="0.2.0",
+    license="MIT",
+    description="Data science library for data science / data analysis teams",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Meshack Kitonga",
+    author_email="dev.kitonga@gmail.com",
+    url="https://github.com/datahelp",
+    packages=find_packages(),
+    install_requires=[
+        "pandas",
+        "scikit-learn",
+        "seaborn",
+        "numpy",
+        "jupyter",
+        "matplotlib",
+        "nltk",
+        "joblib",
+    ],
+    python_requires='>=3.8',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires=">=3.6",
+)
