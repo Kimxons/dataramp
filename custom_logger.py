b4a9332808a8e5aeb624a641298428fde7495357
kimxons <kitongameshack9@gmail.com>
2023-09-23 21:38:27 +0300

    custom log file

---
+++ custom_logger.py
@@ -0,0 +1,44 @@
+import logging
+import os
+import sys
+from logging.handlers import RotatingFileHandler
+
+LOGGING_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'dh_logs')
+
+if not os.path.exists(LOGGING_DIR):
+    os.makedirs(LOGGING_DIR)
+
+
+class Logger:
+    def __init__(self, logger_name=None, filename=None, log_level=logging.DEBUG):  # noqa: E501
+        self.logger = logging.getLogger(logger_name)
+        self.logger.setLevel(log_level)
+        self.logger.propagate = False
+
+        formatter = logging.Formatter(
+            '%(asctime)s | %(levelname)s : %(message)s')
+
+        handler = logging.StreamHandler(sys.stdout)
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+        if filename is not None:
+            log_file = os.path.join(LOGGING_DIR, filename)
+            fhandler = RotatingFileHandler(
+                log_file, maxBytes=1024*1024, backupCount=5)  # 1MB
+            fhandler.setFormatter(formatter)
+            self.logger.addHandler(fhandler)
+
+    def close(self):
+        for handler in self.logger.handlers:
+            handler.close()
+            self.logger.removeHandler(handler)
+
+    def error(self, message, *args, **kwargs):
+        self.logger.error(message, *args, **kwargs)
+
+    def info(self, message, *args, **kwargs):
+        self.logger.info(message, *args, **kwargs)
+
+    def warning(self, message, *args, **kwargs):
+        self.logger.warning(message, *args, **kwargs)
